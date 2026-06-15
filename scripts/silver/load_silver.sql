33829bc20adf67ce84fb312c7da223173df0bf7d
Priyanka <priyankat30@gmail.com>
2026-06-15 17:42:54 +0800

    Loading silver.crm_prd_info

--- scripts/silver/load_silver.sql
+++ scripts/silver/load_silver.sql
@@ -35,4 +35,34 @@ FROM
 ) t
 WHERE flag_last = 1; -- Select the most recent record per customer
 
-SELECT * FROM silver.crm_cust_info
+
+-- Loading silver.crm_prd_info
+INSERT INTO silver.crm_prd_info (
+			prd_id,
+			cat_id,
+			prd_key,
+			prd_nm,
+			prd_cost,
+			prd_line,
+			prd_start_dt,
+			prd_end_dt
+		)
+		SELECT
+			prd_id,
+			REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id, -- Extract category ID
+			SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,        -- Extract product key
+			prd_nm,
+			ISNULL(prd_cost, 0) AS prd_cost,
+			CASE 
+				WHEN UPPER(TRIM(prd_line)) = 'M' THEN 'Mountain'
+				WHEN UPPER(TRIM(prd_line)) = 'R' THEN 'Road'
+				WHEN UPPER(TRIM(prd_line)) = 'S' THEN 'Other Sales'
+				WHEN UPPER(TRIM(prd_line)) = 'T' THEN 'Touring'
+				ELSE 'n/a'
+			END AS prd_line, -- Map product line codes to descriptive values
+			CAST(prd_start_dt AS DATE) AS prd_start_dt,
+			CAST(
+				LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - 1 
+				AS DATE
+			) AS prd_end_dt -- Calculate end date as one day before the next start date
+		FROM bronze.crm_prd_info;
