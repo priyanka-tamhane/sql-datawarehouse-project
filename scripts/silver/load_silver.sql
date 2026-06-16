1961b0d6e09ceaaad2ba635be0083a107143be4f
Priyanka <priyankat30@gmail.com>
2026-06-16 12:13:41 +0800

    Loading silver.erp_loc_a101

--- scripts/silver/load_silver.sql
+++ scripts/silver/load_silver.sql
@@ -124,3 +124,15 @@ CASE WHEN UPPER(TRIM(gen)) IN ('F','FEMALE') THEN 'Female'
 	 ELSE 'n/a'
 END AS gen -- Normalize gender values and handle unknown cases
 from bronze.erp_cust_az12
+
+
+-- Loading silver.erp_loc_a101
+INSERT INTO silver.erp_loc_a101 (cid,cntry)
+SELECT
+REPLACE(cid,'-','') AS cid,
+CASE WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+	 WHEN TRIM(cntry) IN ('US','USA') THEN 'United States'
+	 WHEN TRIM(cntry) ='' OR cntry IS NULL THEN 'n/a'
+	 ELSE TRIM(cntry)
+END AS cntry -- Normalize and handle missing or blank conuntry codes
+FROM bronze.erp_loc_a101 
